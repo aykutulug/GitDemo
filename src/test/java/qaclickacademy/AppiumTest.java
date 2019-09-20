bdc9cd31aeba3fd1183407e3feea74f511dc732a
Aykut <aykutulu@gmail.com>
2019-09-20 11:27:45 -0400

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NatuveAppAndroid() {
+		System.out.println("NatuveAppAndroid");
+	}
+
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+
+}
