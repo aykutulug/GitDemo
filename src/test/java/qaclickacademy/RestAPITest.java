32ad8126c18d982415c14b99ed938df8a01ec583
Aykut <aykutulu@gmail.com>
2019-09-20 12:06:13 -0400

    this is commit from EST timezone

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 public class RestAPITest {
 	@Test
 	public void postJira() {
-		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
 
 	@Test
